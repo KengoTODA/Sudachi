aa88fe24e382952555e991d5faabd01fd74fdd86
Kengo TODA <skypencil@gmail.com>
2021-12-05 21:46:49 +0800

    chore: wrap InputStream with BufferedInputStream
    this change increases throughput from 342.836 [ops/s] to 356.307 [ops/s]

--- src/main/java/com/worksap/nlp/sudachi/dictionary/DictionaryBuilder.java
+++ src/main/java/com/worksap/nlp/sudachi/dictionary/DictionaryBuilder.java
@@ -16,6 +16,7 @@
 
 package com.worksap.nlp.sudachi.dictionary;
 
+import java.io.BufferedInputStream;
 import java.io.Console;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
@@ -106,7 +107,7 @@ public class DictionaryBuilder {
         buffer = byteBuffer; // a kludge for Java 9
     }
 
-    void build(List<String> lexiconPaths, FileInputStream matrixInput, FileOutputStream output) throws IOException {
+    void build(List<String> lexiconPaths, InputStream matrixInput, FileOutputStream output) throws IOException {
         logger.info("reading the source file...");
         for (String path : lexiconPaths) {
             try (FileInputStream lexiconInput = new FileInputStream(path)) {
@@ -210,7 +211,7 @@ public class DictionaryBuilder {
         return posTable.getId(String.join(",", posStrings));
     }
 
-    void writeGrammar(FileInputStream matrixInput, FileChannel output) throws IOException {
+    void writeGrammar(InputStream matrixInput, FileChannel output) throws IOException {
         logger.info("writing the POS table...");
         convertPOSTable(posTable.getList());
         buffer.flip();
@@ -610,7 +611,7 @@ public class DictionaryBuilder {
             output.write(header.toByte());
 
             DictionaryBuilder builder = new DictionaryBuilder();
-            builder.build(lexiconPaths, matrixInput, output);
+            builder.build(lexiconPaths, new BufferedInputStream(matrixInput), output);
         }
     }
 }
